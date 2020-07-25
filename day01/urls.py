a326ef195acae9a1b12d6e1ec5285cfd5c55848e
moka12579 <3511560239@qq.com>
2020-07-25 10:34:37 +0800

    第一天补充

--- day01/urls.py
+++ day01/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, re_path
 
-from day01.views import view_func, view_re_func, view_func_v1, view_template
+from day01.views import *
 
 urlpatterns = [
     path('view/name/', view_func, name='name'),
     path('view/v1/', view_func_v1, name='v1'),
-    path('temp/', view_template, name='template'),
     re_path('^9\d{4}/$', view_re_func),
+    path('temp/', view_template),
+    path('temp_v1/', view_template_2),
 ]
--- day01/views.py
+++ day01/views.py
@@ -1,5 +1,24 @@
+import datetime
+
 from django.shortcuts import render, redirect, reverse
 from django.http.response import HttpResponse
+
+
+
+def hello():
+    return "django"
+
+
+class Foo:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+dc = {
+    'username': 'test',
+    'password': 'test123',
+}
 
 # Create your views here.
 def view_func(request):
@@ -18,4 +37,20 @@ def view_re_func(request):
 
 def view_template(request):
 
-    return render(request, 'demo.html')
+    return render(request, 'demo.html', context={
+        'username': 'test',
+        'foo': [n for n in range(10)],
+        'func': hello(),
+        'cls': Foo('test1', 18),
+        'dc': dc
+    })
+
+def view_template_2(request):
+    return render(request, 'filter.html', context={
+        "name": "",
+        "test": "TEST",
+        "num1": 10,
+        "num2": 20,
+        "lis": [n for n in range(10)],
+        "now": datetime.datetime.now,
+    })
